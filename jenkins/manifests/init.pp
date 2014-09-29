59662c7766e5e9eb7b98ea0b253f35e90d069802
Tully Foote <tfoote@osrfoundation.org>
2014-09-29 16:28:29 -0700

    adding some more jenkins plugins

--- jenkins/manifests/init.pp
+++ jenkins/manifests/init.pp
@@ -2,6 +2,49 @@ include jenkins
 
 
 jenkins::plugin {
+  "ansicolor" : ;
+}
+
+jenkins::plugin {
+  "bazaar" : ;
+}
+
+jenkins::plugin {
+  "build-timeout" : ;
+}
+
+jenkins::plugin {
   "git" : ;
 }
 
+jenkins::plugin {
+  "github-api" : ;
+}
+
+jenkins::plugin {
+  "groovy-postbuild" : ;
+}
+
+jenkins::plugin {
+  "mercurial" : ;
+}
+
+jenkins::plugin {
+  "PrioritySorter" : ;
+}
+
+jenkins::plugin {
+  "ssh-agent" : ;
+}
+
+jenkins::plugin {
+  "subversion" : ;
+}
+
+jenkins::plugin {
+  "token-macro" : ;
+}
+
+jenkins::plugin {
+  "warnings" : ;
+}
