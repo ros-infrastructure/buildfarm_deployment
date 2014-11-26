1fb1db49d1423a5098656d4909d49fc1a29146fd
Tully Foote <tfoote@osrfoundation.org>
2014-11-25 21:57:10 -0800

    Homogenize the deployment.
    Use a standard deploy.bash. And fix master to use site.pp instead of it's unique init.pp.
    
    Use the deploy.bash in the Dockerfile
    
    Copy in content to the same place it would otherwise be if cloned. A little less efficient for docker caching, but much easier for manual installation.

---
+++ slave/deploy.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp hiera.yaml /etc/puppet
+mkdir -p /etc/puppet/hieradata
+cp common.yaml /etc/puppet/hieradata
+
+puppet apply -v manifests/site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd`
