412a8cfb1afd81c31403728989639f6bd4d009af
Tully Foote <tfoote@osrfoundation.org>
2014-11-26 15:05:29 -0800

    removing unneeded prerequisites

--- slave/install_prerequisites.bash
+++ slave/install_prerequisites.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install ruby
 
-# not explicitly called out by jenkins-slave but needed
-apt-get install -y bzr git mercurial subversion
+# needed for installing puppet via gem
+apt-get install -y ruby
 
 gem install puppet --no-rdoc --no-ri
 puppet module install rtyler/jenkins
