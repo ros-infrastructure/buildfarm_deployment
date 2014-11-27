5a92747106d5bb64d89b81b61d78e8d3adf4c0bc
Tully Foote <tfoote@osrfoundation.org>
2014-11-26 19:04:43 -0800

    log puppet to file

--- master/deploy.bash
+++ master/deploy.bash
@@ -4,4 +4,4 @@ cp hiera.yaml /etc/puppet
 mkdir -p /etc/puppet/hieradata
 cp common.yaml /etc/puppet/hieradata
 
-puppet apply -v manifests/site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd`
+puppet apply -v manifests/site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd` -l /var/log/puppet.log
