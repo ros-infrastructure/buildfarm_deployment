331667226ffd0b5a1d57c122488a086ae4617345
Tully Foote <tfoote@osrfoundation.org>
2015-01-07 18:25:47 -0800

    removing unused files. config and docker stuff is in the buildfarm_deployment_config. A few others were left over.

--- build_all.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit;
-
-docker build -t slave slave/
-docker build -t master master/
-docker build -t repo repo/
--- master/deploy.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-cp hiera.yaml /etc/puppet
-mkdir -p /etc/puppet/hieradata
-cp common.yaml /etc/puppet/hieradata
-
-puppet apply -v manifests/site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:`pwd` -l /var/log/puppet.log
