92417680462255cd230e3f29399228b65130f1ab
Dirk Thomas <dthomas@osrfoundation.org>
2015-01-09 09:35:03 -0800

    add qemu-user-static package on slaves

--- slave/manifests/site.pp
+++ slave/manifests/site.pp
@@ -64,6 +64,11 @@ package { 'mercurial':
   ensure => 'installed',
 }
 
+# required for armhf builds
+package { 'qemu-user-static':
+  ensure => 'installed',
+}
+
 package { 'subversion':
   ensure => 'installed',
 }
