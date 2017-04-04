1879c5f01e39a971023a491c4976e69bd335e2a5
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-04 09:39:08 -0400

    Move autoreconfigure logic to osrf::base profile.

--- modules/profile/manifests/jenkins/master.pp
+++ modules/profile/manifests/jenkins/master.pp
@@ -484,24 +484,4 @@ class profile::jenkins::master {
     group => 'jenkins',
     content => template('jenkins_files/credentials.xml.erb'),
   }
-
-
-  if hiera('autoreconfigure') {
-    cron {'autoreconfigure':
-      environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
-      '],
-      command => hiera('autoreconfigure::command'),
-      user    => root,
-      month   => absent,
-      monthday => absent,
-      hour    => absent,
-      minute  => '*/15',
-      weekday => absent,
-    }
-  }
-  else {
-    cron {'autoreconfigure':
-      ensure => absent,
-    }
-  }
 }
--- modules/profile/manifests/osrf/base.pp
+++ modules/profile/manifests/osrf/base.pp
@@ -1,3 +1,21 @@
 class profile::osrf::base {
+  if hiera('autoreconfigure') {
+    cron {'autoreconfigure':
+      environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+      '],
+      command => hiera('autoreconfigure::command'),
+      user    => root,
+      month   => absent,
+      monthday => absent,
+      hour    => absent,
+      minute  => '*/15',
+      weekday => absent,
+    }
+  }
+  else {
+    cron {'autoreconfigure':
+      ensure => absent,
+    }
+  }
 }
 
