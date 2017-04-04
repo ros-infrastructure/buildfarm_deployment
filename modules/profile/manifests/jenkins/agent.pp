94041dd7c96cab29d4c5c8903292e143e91c414b
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-04 09:54:45 -0400

    Temporarily remove agent-on-master specific code.

--- modules/profile/manifests/jenkins/agent.pp
+++ modules/profile/manifests/jenkins/agent.pp
@@ -1,19 +1,5 @@
 class profile::jenkins::agent {
 
-  ### Jenkins Slave
-  ## Don't install java to avoid conflict with the master puppet formula
-  ## https://github.com/jenkinsci/puppet-jenkins/issues/224
-  class { 'jenkins::slave':
-    #labels => 'on_master',
-    slave_mode => 'exclusive',
-    slave_name => 'slave_on_master',
-    slave_user => 'jenkins-agent',
-    manage_slave_user => false,
-    executors => '1',
-    install_java => false,
-    require => User['jenkins-agent'],
-  }
-
   # Must be declared for pip installation module to succeed.
   package {'curl':
     ensure => present,
@@ -34,12 +20,4 @@ class profile::jenkins::agent {
   package { 'bzr':
     ensure => 'installed',
   }
-
-  user{'jenkins-agent':
-    ensure => present,
-    managehome => true,
-    groups => ['docker'],
-    require => Package['docker.io']
-  }
-
 }
