6ff9c194957892f629e7e980229289f842844d4b
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-05 13:32:39 -0400

    Remove spurious autoreconfigure setup.

--- modules/profile/manifests/osrf/repo.pp
+++ modules/profile/manifests/osrf/repo.pp
@@ -179,26 +179,6 @@ class profile::osrf::repo {
     require    => File['/home/jenkins-agent/.ssh/gpg_private_key.sec']
   }
 
-  if hiera('autoreconfigure') {
-    cron {'autoreconfigure':
-      environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
-      '],
-      command => hiera('autoreconfigure::command'),
-      user    => root,
-      month   => absent,
-      monthday => absent,
-      hour    => absent,
-      minute  => '*/15',
-      weekday => absent,
-    }
-  }
-  else {
-    cron {'autoreconfigure':
-      ensure => absent,
-    }
-  }
-
-
   exec {"init_building_repo":
     path        => '/bin:/usr/bin',
     command     => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_building -c',
