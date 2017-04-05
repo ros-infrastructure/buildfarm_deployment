5b7e744aaff55746df1df90a7be01b3d314139cf
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-05 16:04:12 -0400

    Factor username into class parameter.

--- modules/profile/manifests/jenkins/agent.pp
+++ modules/profile/manifests/jenkins/agent.pp
@@ -1,51 +1,56 @@
-class profile::jenkins::agent {
+class profile::jenkins::agent (
+  String $agent_username = 'jenkins-agent',
+) {
 
-  # Must be declared for pip installation module to succeed.
-  package {'curl':
+  user { "$agent_username" :
     ensure => present,
+    managehome => true,
+    groups => ['docker'],
+    # Docker is required so the docker group is created.
+    # We could technically depend on the docker group but this ensures the docker group id is the one
+    # preferred by the docker installation.
+    require => Class['docker'],
   }
 
   # Setup SSH known hosts
-  file { '/home/jenkins-agent/.ssh/' :
+  file { "/home/$agent_username/.ssh/" :
     ensure => 'directory',
     mode   => '644',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
-    require => User['jenkins-agent'],
+    owner  => $agent_username,
+    group  => $agent_username,
+    require => User["$agent_username"],
   }
 
-  file { '/home/jenkins-agent/.ssh/known_hosts':
+  file { "/home/$agent_username/.ssh/known_hosts":
     ensure => 'present',
     mode => '0644',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
+    owner => $agent_username,
+    group => $agent_username,
     content => template('agent_files/known_hosts.erb'),
-    require => File['/home/jenkins-agent/.ssh/'],
+    require => File["/home/$agent_username/.ssh/"],
   }
 
   class { 'jenkins::slave':
     labels => 'buildslave',
     slave_mode => 'exclusive',
-    slave_user => 'jenkins-agent',
+    slave_user => $agent_username,
     manage_slave_user => false,
     executors => hiera('jenkins::agent::num_executors', 1),
-    require => User['jenkins-agent'],
-  }
-
-  user{'jenkins-agent':
-    ensure => present,
-    managehome => true,
-    groups => ['docker'],
-    require => Class['docker']
+    require => User[$agent_username],
   }
 
   # Make sure this directory exists so it can be mounted.
-  file { '/home/jenkins-agent/.ccache' :
+  file { "/home/$agent_username/.ccache" :
     ensure => 'directory',
     mode   => '644',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
-    require => User['jenkins-agent'],
+    owner  => $agent_username,
+    group  => $agent_username,
+    require => User[$agent_username],
+  }
+
+  # Must be declared for pip installation module to succeed.
+  package {'curl':
+    ensure => present,
   }
 
   # required by cleanup_docker_images.py
@@ -84,13 +89,11 @@ class profile::jenkins::agent {
     ensure => 'installed',
   }
 
-  ### install latest docker
-  class {'docker':
-  }
-
   package { 'python3-pip':
     ensure => 'installed',
   }
+
+  include docker
 
   # required by cleanup_docker script
   pip::install { 'docker-py':
@@ -102,23 +105,23 @@ class profile::jenkins::agent {
   }
 
   # script to clean up docker images from oldest
-  file { '/home/jenkins-agent/cleanup_docker_images.py':
+  file { "/home/$agent_username/cleanup_docker_images.py":
     mode => '0774',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
+    owner => $agent_username,
+    group => $agent_username,
     source => 'puppet:///modules/agent_files/home/jenkins-agent/cleanup_docker_images.py',
-    require => User['jenkins-agent'],
+    require => User[$agent_username],
   }
 
   # clean up containers and dangling images https://github.com/docker/docker/issues/928#issuecomment-58619854
   cron {'docker_cleanup_images':
-    command => 'bash -c "python3 -u /home/jenkins-agent/cleanup_docker_images.py --minimum-free-percent 10 --minimum-free-space 50"',
-    user    => 'jenkins-agent',
+    command => "bash -c \"python3 -u /home/$agent_username/cleanup_docker_images.py --minimum-free-percent 10 --minimum-free-space 50\"",
+    user    => $agent_username,
     month   => absent,
     monthday => absent,
     hour    => '*',
     minute  => '*/15',
     weekday => absent,
-    require => User['jenkins-agent'],
+    require => User[$agent_username],
   }
 }
