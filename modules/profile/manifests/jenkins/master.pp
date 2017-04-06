045ee1f3f232c90ea9f6a48c4888a0b0732f7a3c
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-06 10:25:01 -0400

    Add comments for future context.

--- modules/profile/manifests/jenkins/master.pp
+++ modules/profile/manifests/jenkins/master.pp
@@ -3,7 +3,6 @@ class profile::jenkins::master {
 
   include jenkins_files
 
-  ## Jenkins plugins
   ### Jenkins Plugins
 
   # required by workflow-cps
@@ -432,11 +431,12 @@ class profile::jenkins::master {
 
 
   ### install latest docker
-
-  class {'docker':
-  }
+  include docker
 
   # Add jenkins user to docker group if not already
+  # That this needs to be done with a command rather than a resource declaration is a side effect of the 'jenkins'
+  # user being managed by the jenkins module.
+  # If future alterations are necessary it may be worth managing the user ourselves.
   exec {'jenkins docker membership':
     unless => '/bin/bash -c "/usr/bin/id -nG jenkins | /bin/grep -wq docker"',
     command => '/usr/sbin/usermod -aG docker jenkins',
