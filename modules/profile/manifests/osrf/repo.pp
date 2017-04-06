9b91fc73773c7ea990ad3feb1994472beaf6e8ec
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-06 11:39:31 -0400

    Use jenkins agent username variable from agent profile.

--- modules/profile/manifests/osrf/repo.pp
+++ modules/profile/manifests/osrf/repo.pp
@@ -1,4 +1,8 @@
 class profile::osrf::repo {
+  require profile::jenkins::agent
+  # This is not a class parameter so it cannot be overloaded separately from the jenkins::agent value.
+  $agent_username = $profile::jenkins::agent::agent_username
+
   package {'git':
     ensure => 'installed',
   }
@@ -36,21 +40,21 @@ class profile::osrf::repo {
   ### install latest docker
   include docker
 
-  file { '/home/jenkins-agent/.ssh':
+  file { "/home/${agent_username}/.ssh":
     ensure => 'directory',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
+    owner  => $agent_username,
+    group  => $agent_username,
     mode   => '0700',
-    require => User['jenkins-agent'],
+    require => User[$agent_username],
   }
 
   file { '/var/repos/docs':
     ensure => 'directory',
     mode   => '0755',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
+    owner  => $agent_username,
+    group  => $agent_username,
     require => [
-      User['jenkins-agent'],
+      User[$agent_username],
       File['/var/repos'],
     ]
   }
@@ -58,10 +62,10 @@ class profile::osrf::repo {
   file { '/var/repos/rosdistro_cache':
     ensure => 'directory',
     mode   => '0755',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
+    owner  => $agent_username,
+    group  => $agent_username,
     require => [
-      User['jenkins-agent'],
+      User[$agent_username],
       File['/var/repos'],
     ]
   }
@@ -69,10 +73,10 @@ class profile::osrf::repo {
   file { '/var/repos/status_page':
     ensure => 'directory',
     mode   => '0755',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
+    owner  => $agent_username,
+    group  => $agent_username,
     require => [
-      User['jenkins-agent'],
+      User[$agent_username],
       File['/var/repos'],
     ]
   }
@@ -83,28 +87,28 @@ class profile::osrf::repo {
   file { $repo_dirs :
     ensure => 'directory',
     mode   => '0644',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
-    require => User['jenkins-agent'],
+    owner  => $agent_username,
+    group  => $agent_username,
+    require => User[$agent_username],
   }
 
-  $config_dirs = ['/home/jenkins-agent/.buildfarm',
+  $config_dirs = ["/home/${agent_username}/.buildfarm",
   ]
 
   file { $config_dirs :
     ensure => 'directory',
     mode   => '0644',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
-    require => User['jenkins-agent'],
+    owner  => $agent_username,
+    group  => $agent_username,
+    require => User[$agent_username],
   }
 
-  file { '/home/jenkins-agent/.buildfarm/reprepro-updater.ini':
+  file { "/home/${agent_username}/.buildfarm/reprepro-updater.ini":
     mode => '0600',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
+    owner  => $agent_username,
+    group  => $agent_username,
     content => hiera('jenkins-agent::reprepro_updater_config'),
-    require => File['/home/jenkins-agent/.buildfarm'],
+    require => File["/home/${agent_username}/.buildfarm"],
   }
 
   # Set up apache
@@ -119,129 +123,128 @@ class profile::osrf::repo {
     priority   => '10',
     #  servername => 'localhost',
     #  require    => Reprepro::Distribution['precise'],
-  }
+    }
 
 
-  # Manually managing reprepro, the puppet formula is too fragile wrt to user management.
-  package {'reprepro':
-    ensure => 'installed',
-  }
+    # Manually managing reprepro, the puppet formula is too fragile wrt to user management.
+    package {'reprepro':
+      ensure => 'installed',
+    }
 
-  #needed by reprepro-updater
-  package {'python-configparser':
-    ensure => 'installed',
-  }
-  # GPG key management
+    #needed by reprepro-updater
+    package {'python-configparser':
+      ensure => 'installed',
+    }
+    # GPG key management
 
-  file { '/home/jenkins-agent/.ssh/gpg_private_key.sec':
-    mode => '0600',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
-    content => hiera('jenkins-agent::gpg_private_key'),
-    require => File['/home/jenkins-agent/.ssh'],
-  }
+    file { "/home/${agent_username}/.ssh/gpg_private_key.sec":
+      mode => '0600',
+      owner  => $agent_username,
+      group  => $agent_username,
+      content => hiera('jenkins-agent::gpg_private_key'),
+      require => File["/home/${agent_username}/.ssh"],
+    }
 
-  file { '/home/jenkins-agent/.ssh/gpg_public_key.pub':
-    mode => '0644',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
-    content => hiera('jenkins-agent::gpg_public_key'),
-    require => File['/home/jenkins-agent/.ssh'],
-  }
+    file { "/home/${agent_username}/.ssh/gpg_public_key.pub":
+      mode => '0644',
+      owner  => $agent_username,
+      group  => $agent_username,
+      content => hiera('jenkins-agent::gpg_public_key'),
+      require => File["/home/${agent_username}/.ssh"],
+    }
 
-  file { '/var/repos/repos.key':
-    mode => '0644',
-    owner => 'jenkins-agent',
-    group => 'jenkins-agent',
-    content => hiera('jenkins-agent::gpg_public_key'),
-    require => File['/var/repos'],
-  }
+    file { '/var/repos/repos.key':
+      mode => '0644',
+      owner  => $agent_username,
+      group  => $agent_username,
+      content => hiera('jenkins-agent::gpg_public_key'),
+      require => File['/var/repos'],
+    }
 
-  $gpg_key_id = hiera('jenkins-agent::gpg_key_id')
+    $gpg_key_id = hiera('jenkins-agent::gpg_key_id')
 
-  exec { 'import_public_key':
-    path        => '/bin:/usr/bin',
-    command     => 'gpg --import /home/jenkins-agent/.ssh/gpg_public_key.pub',
-    user        => 'jenkins-agent',
-    group       => 'jenkins-agent',
-    unless      => "gpg --list-keys | grep ${gpg_key_id}",
-    logoutput   => on_failure,
-    require    => File['/home/jenkins-agent/.ssh/gpg_public_key.pub']
-  }
+    exec { 'import_public_key':
+      path        => '/bin:/usr/bin',
+      command     => "gpg --import /home/${agent_username}/.ssh/gpg_public_key.pub",
+      user  => $agent_username,
+      group  => $agent_username,
+      unless      => "gpg --list-keys | grep ${gpg_key_id}",
+      logoutput   => on_failure,
+      require    => File["/home/${agent_username}/.ssh/gpg_public_key.pub"]
+    }
 
-  exec { 'import_private_key':
-    path        => '/bin:/usr/bin:',
-    command     => 'gpg --import /home/jenkins-agent/.ssh/gpg_private_key.sec',
-    user        => 'jenkins-agent',
-    group       => 'jenkins-agent',
-    unless      => "gpg -K | grep ${gpg_key_id}",
-    logoutput   => on_failure,
-    require    => File['/home/jenkins-agent/.ssh/gpg_private_key.sec']
-  }
+    exec { 'import_private_key':
+      path        => '/bin:/usr/bin:',
+      command     => "gpg --import /home/${agent_username}/.ssh/gpg_private_key.sec",
+      user  => $agent_username,
+      group  => $agent_username,
+      unless      => "gpg -K | grep ${gpg_key_id}",
+      logoutput   => on_failure,
+      require    => File["/home/${agent_username}/.ssh/gpg_private_key.sec"]
+    }
 
-  exec {'init_building_repo':
-    path        => '/bin:/usr/bin',
-    command     => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_building -c',
-    environment => ['PYTHONPATH=/home/jenkins-agent/reprepro-updater/src:$PYTHONPATH'],
-    user        => 'jenkins-agent',
-    group       => 'jenkins-agent',
-    unless      => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_building -q',
-    logoutput   => on_failure,
-    require     => [
-      Vcsrepo['/home/jenkins-agent/reprepro-updater'],
-      File['/home/jenkins-agent/.buildfarm/reprepro-updater.ini'],
-    ]
-  }
+    exec {'init_building_repo':
+      path        => '/bin:/usr/bin',
+      command     => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_building -c",
+      environment => ["PYTHONPATH=/home/${agent_username}/reprepro-updater/src:\$PYTHONPATH"],
+      user  => $agent_username,
+      group  => $agent_username,
+      unless      => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_building -q",
+      logoutput   => on_failure,
+      require     => [
+        Vcsrepo["/home/${agent_username}/reprepro-updater"],
+        File["/home/${agent_username}/.buildfarm/reprepro-updater.ini"],
+      ]
+    }
 
-  exec {'init_testing_repo':
-    path        => '/bin:/usr/bin',
-    command     => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_testing -c',
-    environment => ['PYTHONPATH=/home/jenkins-agent/reprepro-updater/src:$PYTHONPATH'],
-    user        => 'jenkins-agent',
-    group       => 'jenkins-agent',
-    unless      => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_testing -q',
-    logoutput   => on_failure,
-    require     => [
-      Vcsrepo['/home/jenkins-agent/reprepro-updater'],
-      File['/home/jenkins-agent/.buildfarm/reprepro-updater.ini'],
-    ]
-  }
+    exec {'init_testing_repo':
+      path        => '/bin:/usr/bin',
+      command     => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_testing -c",
+      environment => ["PYTHONPATH=/home/${agent_username}/reprepro-updater/src:\$PYTHONPATH"],
+      user  => $agent_username,
+      group  => $agent_username,
+      unless      => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_testing -q",
+      logoutput   => on_failure,
+      require     => [
+        Vcsrepo["/home/${agent_username}/reprepro-updater"],
+        File["/home/${agent_username}/.buildfarm/reprepro-updater.ini"],
+      ]
+    }
 
-  exec {'init_main_repo':
-    path        => '/bin:/usr/bin',
-    command     => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_main -c',
-    environment => ['PYTHONPATH=/home/jenkins-agent/reprepro-updater/src:$PYTHONPATH'],
-    user        => 'jenkins-agent',
-    group       => 'jenkins-agent',
-    unless      => '/home/jenkins-agent/reprepro-updater/scripts/setup_repo.py ubuntu_testing -q',
-    logoutput   => on_failure,
-    require     => [
-      Vcsrepo['/home/jenkins-agent/reprepro-updater'],
-      File['/home/jenkins-agent/.buildfarm/reprepro-updater.ini'],
-    ]
-  }
-  # needed for boostrapping the repo
-  vcsrepo { '/home/jenkins-agent/reprepro-updater':
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/ros-infrastructure/reprepro-updater.git',
-    revision => 'refactor',
-    user     => 'jenkins-agent',
-    require => User['jenkins-agent'],
-  }
+    exec {'init_main_repo':
+      path        => '/bin:/usr/bin',
+      command     => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_main -c",
+      environment => ["PYTHONPATH=/home/${agent_username}/reprepro-updater/src:\$PYTHONPATH"],
+      user  => $agent_username,
+      group  => $agent_username,
+      unless      => "/home/${agent_username}/reprepro-updater/scripts/setup_repo.py ubuntu_testing -q",
+      logoutput   => on_failure,
+      require     => [
+        Vcsrepo["/home/${agent_username}/reprepro-updater"],
+        File["/home/${agent_username}/.buildfarm/reprepro-updater.ini"],
+      ]
+    }
+    # needed for boostrapping the repo
+    vcsrepo { "/home/${agent_username}/reprepro-updater":
+      ensure   => latest,
+      provider => git,
+      source   => 'https://github.com/ros-infrastructure/reprepro-updater.git',
+      revision => 'refactor',
+      user     => $profile::jenkins::agent::username,
+      require => User[$profile::jenkins::agent::username],
+    }
 
+    # Create directory for reprepro_config
+    file { "/home/${agent_username}/reprepro_config":
+      ensure => 'directory',
+      owner  => $agent_username,
+      group  => $agent_username,
+      mode   => '0700',
+      require => User[$profile::jenkins::agent::username],
+    }
 
-  # Create directory for reprepro_config
-  file { '/home/jenkins-agent/reprepro_config':
-    ensure => 'directory',
-    owner  => 'jenkins-agent',
-    group  => 'jenkins-agent',
-    mode   => '0700',
-    require => User['jenkins-agent'],
-  }
-
-  # Pull reprepro updater
-  if hiera('jenkins-agent::reprepro_config', false){
-    create_resources(file, hiera('jenkins-agent::reprepro_config'))
-  }
+    # Pull reprepro updater
+    if hiera('jenkins-agent::reprepro_config', false){
+      create_resources(file, hiera('jenkins-agent::reprepro_config'))
+    }
 }
