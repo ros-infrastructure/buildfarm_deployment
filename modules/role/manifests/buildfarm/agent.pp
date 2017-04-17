e991d9024f83d45813ecab8ea735750cac520004
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-17 10:53:21 -0400

    Move site manifests into roles.

---
+++ modules/role/manifests/buildfarm/agent.pp
@@ -0,0 +1,47 @@
+class role::buildfarm::agent {
+  # Find the other instances
+  if hiera('repo::ip', false) {
+    host {'repo':
+      ip => hiera('repo::ip'),
+    }
+  }
+  else {
+    host {'repo':
+      ensure => absent,
+    }
+  }
+
+  if hiera('master::ip', false) {
+    host {'master':
+      ip => hiera('master::ip'),
+    }
+  }
+  else {
+    host {'master':
+      ensure => absent,
+    }
+  }
+
+  # bring in classes listed in hiera
+  if hiera('classes', false) {
+    hiera_include('classes')
+  }
+
+  # Setup generic ssh_keys
+  if hiera('ssh_keys', false){
+    $defaults = {
+      'ensure' => 'present',
+    }
+    create_resources(ssh_authorized_key, hiera('ssh_keys'), $defaults)
+  }
+  else{
+    notice("No ssh_keys defined. You should probably have at least one.")
+  }
+
+  include profile::osrf::base
+  include profile::jenkins::agent
+
+  if hiera('run_squid', false) {
+    include profile::squidinacan
+  }
+}
