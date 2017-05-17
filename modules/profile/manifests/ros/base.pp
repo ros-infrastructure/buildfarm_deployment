e799ef84ad22a44af05bfe10fdc311c6249c2480
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-05-17 13:27:11 -0700

    Move host file updates into base profile.

--- modules/profile/manifests/ros/base.pp
+++ modules/profile/manifests/ros/base.pp
@@ -3,6 +3,45 @@ class profile::ros::base {
   # setup ntp with defaults
   include '::ntp'
 
+  if hiera('repo::ip', false) {
+    host {'repo':
+      ip => hiera('repo::ip'),
+    }
+  }
+  else {
+    host {'repo':
+      ensure => absent,
+    }
+  }
+
+  if hiera('master::ip', false) {
+    host {'master':
+      ip => hiera('master::ip'),
+    }
+  }
+  else {
+    host {'master':
+      ensure => absent,
+    }
+  }
+
+  # bring in classes listed in hiera
+  if hiera('classes', false) {
+    hiera_include('classes')
+  }
+
+  # Setup generic ssh_keys
+  if hiera('ssh_keys', false){
+    $defaults = {
+      'ensure' => 'present',
+    }
+    create_resources(ssh_authorized_key, hiera('ssh_keys'), $defaults)
+  }
+  else{
+    notice("No ssh_keys defined. You should probably have at least one.")
+  }
+
+
   if hiera('autoreconfigure') {
     cron {'autoreconfigure':
       environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
--- modules/role/manifests/buildfarm/repo.pp
+++ modules/role/manifests/buildfarm/repo.pp
@@ -1,33 +1,4 @@
 class role::buildfarm::repo {
-  # bring in classes listed in hiera
-  if hiera('classes', false) {
-    hiera_include('classes')
-  }
-
-
-  # Find the other instances
-  if hiera('master::ip', false) {
-    host {'master':
-      ip => hiera('master::ip'),
-    }
-  }
-  else {
-    host {'master':
-      ensure => absent,
-    }
-  }
-
-  # Setup generic ssh_keys
-  if hiera('ssh_keys', false){
-    $defaults = {
-      'ensure' => 'present',
-    }
-    create_resources(ssh_authorized_key, hiera('ssh_keys'), $defaults)
-  }
-  else{
-    notice("No ssh_keys defined. You need at least one for jenkins-agent.")
-  }
-
   include profile::ros::base
   include profile::ros::repo
 }
