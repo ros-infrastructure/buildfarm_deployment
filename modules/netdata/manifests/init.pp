6d6c76e74d2e57066b10d0f688aea6a6a897402f
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-09-05 14:18:46 -0400

    Add a module for installing and configuring netdata.
    At some point this should be cleaned up, factored out, and released as a
    separate puppet module. Can't be bothered now though.

---
+++ modules/netdata/manifests/init.pp
@@ -0,0 +1,21 @@
+class netdata {
+  file { '/tmp/netdata-kickstart.sh':
+    source => 'puppet:///modules/netdata/netdata-kickstart.sh',
+  }
+
+  exec { 'install-netdata':
+    command => '/tmp/netdata-kickstart.sh --no-updates --non-interactive',
+    require => File['/tmp/netdata-kickstart.sh'],
+    creates => '/usr/local/bin/netdata',
+  }
+
+  file { '/etc/systemd/system/netdata.service':
+    source => 'puppet:///modules/netdata/netdata.service',
+    notify =>  Exec['/bin/systemctl daemon-reload'],
+  }
+
+  service {'netdata':
+    ensure  => running,
+    enable  => true,
+    require =>  [File['/etc/systemd/system/netdata.service'],Exec['install-netdata']],
+}
