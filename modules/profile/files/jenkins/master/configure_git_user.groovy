a4a928404e56b69f4745d343c69cc5b20c89794a
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-01-22 11:14:18 -0800

    Import Jenkins model.
    At the web UI script console this class is imported automatically.
    Not so for the CLI apparently.

--- modules/profile/files/jenkins/master/configure_git_user.groovy
+++ modules/profile/files/jenkins/master/configure_git_user.groovy
@@ -3,8 +3,9 @@
 /* This script is designed to be run via jenkins-cli.jar in order to update the
  * configuration for the Jenkins GitSCM plugin.
  */
+import jenkins.model.Jenkins
 
-def gitscm_config = Jenkins.getInstance().getDescriptor("hudson.plugins.git.GitSCM")
+def gitscm_config = jenkins.model.Jenkins.getInstance().getDescriptor("hudson.plugins.git.GitSCM")
 gitscm_config.setCreateAccountBasedOnEmail(false)
 gitscm_config.setGlobalConfigName("jenkins")
 gitscm_config.setGlobalConfigEmail("jenkins@build.ros.org")
