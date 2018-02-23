5c4de897e0b329aff505f566fce76d176a482025
Jon Azpiazu <jonazpiazu@users.noreply.github.com>
2018-02-23 19:13:32 +0100

    fixing newlines when generating squid-in-a-can service file (#175)

--- modules/profile/manifests/squidinacan.pp
+++ modules/profile/manifests/squidinacan.pp
@@ -20,15 +20,16 @@ class profile::squidinacan {
 
   $disk_cache_size = hiera('squid-in-a-can::max_cache_size', 5000)
   $max_cache_object = hiera('squid-in-a-can::max_cache_object', 1000)
+
   docker::run {'squid-in-a-can':
     image   => 'jpetazzo/squid-in-a-can',
     command => '/tmp/deploy_squid.py',
     env     => ["DISK_CACHE_SIZE=${disk_cache_size}",
                 "MAX_CACHE_OBJECT=${max_cache_object}",
-                'SQUID_DIRECTIVES=\'
-refresh_pattern . 0 0 1 refresh-ims
-refresh_all_ims on # make sure we do not get out of date content #41
-ignore_expect_100 on # needed for new relic system monitor
+                'SQUID_DIRECTIVES=\' \\
+refresh_pattern . 0 0 1 refresh-ims \
+refresh_all_ims on # make sure we do not get out of date content #41 \
+ignore_expect_100 on # needed for new relic system monitor \
 \''],
     volumes => ['/var/cache/squid-in-a-can:/var/cache/squid3',
                 '/var/log/squid-in-a-can:/var/log/squid3',
