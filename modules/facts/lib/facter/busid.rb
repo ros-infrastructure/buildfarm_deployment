3f32d61aef1436912f127e974f9dec054b4eaa14
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-04-26 16:16:33 +0200

    Change busid by gpu_device_bus_id

--- modules/facts/lib/facter/busid.rb
+++ modules/facts/lib/facter/busid.rb
@@ -1,4 +1,4 @@
-Facter.add(:busid) do
+Facter.add(:gpu_device_bus_id) do
   setcode do
     Facter::Core::Execution.execute('nvidia-xconfig --query-gpu-info  | grep BusID | sed "s/.*PCI:/PCI:/g"')
   end
