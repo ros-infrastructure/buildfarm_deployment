8ce1de07ee2c3204c56bb9d7d5176df54c0f2db7
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-04-30 11:38:13 +0200

    master: extend vhost setup to append 'nocanon' to proxy config.

--- modules/profile/manifests/jenkins/master.pp
+++ modules/profile/manifests/jenkins/master.pp
@@ -223,7 +223,7 @@ class profile::jenkins::master {
     docroot => '/var/www.html',
     port    => '80',
     proxy_pass => [
-      { 'path' => '/', 'url' => 'http://localhost:8080/'},
+      { 'path' => '/', 'url' => 'http://localhost:8080/', 'keywords' => ['nocanon'] },
     ],
   }
 
