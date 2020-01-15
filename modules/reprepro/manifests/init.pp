7e23967b5aa2598cf7347d252773883440e8c20e
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2020-01-15 11:53:46 -0800

    Upgrade to reprepro 5.3.0 to resolve Missing 'Binary' field for Ubuntu 20.04, Debian Buster (#227)
    * Update reprepro build script to 5.3.0 from Disco.
    
    * Install reprepro 5.3.0 instead of 5.1.1.
    
    * Install runtime dependencies rather than build dependencies.
    
    Due to an oversight when setting up this module the build dependencies
    were being installed rather than the runtime deps.Systems with the build
    dependencies installed were running fine but reprepro does not require
    the development versions or debhelper to run so this change hopes to
    avoid further confusion.
    
    * Remove reprepro 5.1.1 binary which is no longer used.
    
    * Fix typo in package name.

--- modules/reprepro/manifests/init.pp
+++ modules/reprepro/manifests/init.pp
@@ -1,37 +1,40 @@
 class reprepro {
     # Manually managing reprepro, the puppet formula is too fragile wrt to user management.
-    package { 'debhelper':
+    package { 'libarchive13':
       ensure => present,
     }
-    package { 'libarchive-dev':
+    package { 'libbz2-1.0':
       ensure => present,
     }
-    package { 'libbz2-dev':
+    package { 'libc6':
       ensure => present,
     }
-    package { 'libdb-dev':
+    package { 'libdb5.3':
       ensure => present,
     }
-    package { 'libgpgme11-dev':
+    package { 'libgpg-error0':
       ensure => present,
     }
-    package { 'liblzma-dev':
+    package { 'libgpgme11':
       ensure => present,
     }
-    package { 'libz-dev':
+    package { 'liblzma5':
+      ensure => present,
+    }
+    package { 'zlib1g':
       ensure => present,
     }
 
     exec { 'install-prebuilt-reprepro-deb':
-      command   => '/usr/bin/dpkg -i /tmp/reprepro_5.1.1-1_amd64.deb',
-      unless    => '/usr/bin/dpkg-query -l reprepro | tail -1 | grep -qs 5.1.1-1',
+      command   => '/usr/bin/dpkg -i /tmp/reprepro_5.3.0-1_amd64.deb',
+      unless    => '/usr/bin/dpkg-query -l reprepro | tail -1 | grep -qs 5.3.0-1',
       logoutput => on_failure,
-      require   => [ File['/tmp/reprepro_5.1.1-1_amd64.deb'], Package['debhelper'], Package['libarchive-dev'],
-                    Package['libbz2-dev'], Package['libdb-dev'], Package['libgpgme11-dev'], Package['liblzma-dev'],
-                    Package['libz-dev'] ]
+      require   => [ File['/tmp/reprepro_5.3.0-1_amd64.deb'], Package['libarchive13'], Package['libbz2-1.0'],
+                    Package['libc6'], Package['libdb5.3'], Package['libgpg-error0'], Package['libgpgme11'],
+                    Package['liblzma5'], Package['zlib1g'] ]
     }
 
-    file { '/tmp/reprepro_5.1.1-1_amd64.deb':
-      source => 'puppet:///modules/reprepro/reprepro_5.1.1-1_amd64.deb',
+    file { '/tmp/reprepro_5.3.0-1_amd64.deb':
+      source => 'puppet:///modules/reprepro/reprepro_5.3.0-1_amd64.deb',
     }
 }
