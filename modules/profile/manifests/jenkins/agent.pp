9ab2934252a52125b748050f280b06d300bfc078
Scott K Logan <logans@cottsay.net>
2020-04-01 16:50:27 -0700

    Update the pulp client package versions

--- modules/profile/manifests/jenkins/agent.pp
+++ modules/profile/manifests/jenkins/agent.pp
@@ -124,13 +124,13 @@ class profile::jenkins::agent (
   if hiera('jenkins-agent::pulp_config', false) {
     # required by pulp_upload and pulp_publish scripts
     python::pip { 'pulpcore_client':
-      ensure  => '3.0.0',
+      ensure  => '3.2.0',
       pkgname => 'pulpcore-client',
     }
 
     if hiera('jenkins-agent::pulp_config', {})['rpm'] {
       python::pip { 'pulp_rpm_client':
-        ensure  => '3.0.0',
+        ensure  => '3.2.1',
         pkgname => 'pulp-rpm-client',
       }
     }
