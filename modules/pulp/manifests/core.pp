7b8f73a37d9fd0a8cc6bd8adbbec09feeaa9b604
Scott K Logan <logans@cottsay.net>
2020-04-01 16:14:24 -0700

    Move psql and redis out of docker, split pulp processes

--- modules/pulp/manifests/core.pp
+++ modules/pulp/manifests/core.pp
@@ -2,43 +2,227 @@ define pulp::core (
   $data_dir,
   $admin_password,
 ) {
+  user { 'pulp':
+    ensure => present,
+    managehome => true,
+    uid => 1200,
+  }
+
   file { $data_dir:
     ensure => 'directory',
-    owner  => 1000,
+    owner  => 'pulp',
     mode   => '0700',
+    require => User['pulp'],
+  }
+
+  file { "${data_dir}/media":
+    ensure => 'directory',
+    owner => 'pulp',
+    require => [
+      File[$data_dir],
+      User['pulp'],
+    ],
+  }
+
+  #
+  # Configure Redis
+  #
+
+  package { 'redis-server':
+    ensure => present,
+  }
+
+  service { 'redis':
+    name => 'redis-server',
+    ensure => running,
+    enable => true,
+    require => Package['redis-server'],
+  }
+
+  exec { 'redis_config':
+    path => ['/bin', '/usr/bin'],
+    command => "sed -i -E -e 's|^#? ?port .*|port 0|' -e 's|^#? ?unixsocket .*|unixsocket /var/run/redis/redis.sock|' -e 's|^#? ?unixsocketperm .*|unixsocketperm 777|' /etc/redis/redis.conf",
+    require => Package['redis-server'],
+    notify => Service['redis'],
+  }
+
+  #
+  # Configure Postgres
+  #
+
+  package { 'postgresql-server':
+    name => 'postgresql',
+    ensure => present,
+  }
+
+  service { 'postgresql':
+    ensure => running,
+    enable => true,
+    require => Package['postgresql-server'],
+  }
+
+  exec { 'postgres_user':
+    path => ['/bin', '/usr/bin'],
+    command => 'psql -tc "SELECT 1 FROM pg_user WHERE usename = \'pulp\'" | grep -q 1 || psql -c "CREATE USER pulp WITH SUPERUSER LOGIN"',
+    user => 'postgres',
+    require => [
+      Service['postgresql'],
+      User['pulp'],
+    ],
+  }
+
+  exec { 'postgres_db':
+    path => ['/bin', '/usr/bin'],
+    command => 'psql -tc "SELECT 1 FROM pg_database WHERE datname = \'pulp\'" | grep -q 1 || psql -c "CREATE DATABASE pulp OWNER pulp"',
+    user => 'postgres',
+    require => Exec['postgres_user'],
+  }
+
+  #
+  # Build the Docker image
+  #
+
+  file { "${data_dir}/Dockerfile":
+    ensure => 'file',
+    source => 'puppet:///modules/pulp/Dockerfile',
+    require => File[$data_dir],
   }
 
   include docker
 
   docker::image {'pulp_image':
     ensure => 'latest',
-    docker_dir => '/tmp/pulp/',
+    docker_dir => $data_dir,
     require => [
-      File['/tmp/pulp/Dockerfile'],
-      File['/tmp/pulp/entry_point.sh'],
+      File["${data_dir}/Dockerfile"],
     ],
   }
 
-  # Initialize and set the admin password
-  # docker::run doesn't have a way to NOT detach, so we exec
-  exec { 'pulp_init':
+  #
+  # Initialize pulp
+  #
+  # Note that docker::run doesn't have a way to NOT detach, so we use Exec
+  #
+
+  # Django migrations
+  exec { 'pulp_django_migration':
     path => ['/bin', '/usr/bin'],
-    command => "docker run --rm -v ${data_dir}:/var/lib/pulp/data pulp_image django-admin reset-admin-password -p ${admin_password}",
+    command => "docker run --user 1200 --rm -v /var/run/postgresql:/var/run/postgresql pulp_image pulpcore-manager migrate --noinput",
     require => [
-      File[$data_dir],
       Docker::Image['pulp_image'],
+      Exec['postgres_db'],
+      User['pulp'],
     ],
   }
 
-  docker::run {'pulp':
+  # Reset the admin password
+  exec { 'pulp_admin_pw':
+    path => ['/bin', '/usr/bin'],
+    command => "docker run --user 1200 --rm -v /var/run/postgresql:/var/run/postgresql pulp_image pulpcore-manager reset-admin-password -p ${admin_password}",
+    require => [
+      Docker::Image['pulp_image'],
+      Exec['pulp_django_migration'],
+      User['pulp'],
+    ],
+  }
+
+  # Collect static content
+  exec { 'pulp_collect_static':
+    path => ['/bin', '/usr/bin'],
+    command => "docker run --user 1200 --rm -v ${data_dir}:/var/repos/.pulp pulp_image pulpcore-manager collectstatic --noinput",
+    require => [
+      Docker::Image['pulp_image'],
+      File[$data_dir],
+      User['pulp'],
+    ],
+  }
+
+  #
+  # Configure pulp services
+  #
+
+  docker::run {'pulp_api':
     image => 'pulp_image',
     restart_service => true,
-    volumes => ["${data_dir}:/var/lib/pulp/data"],
-    ports => ['24816:24816', '24817:24817'],
-    require => [
-      File[$data_dir],
-      Docker::Image['pulp_image'],
-      Exec['pulp_init'],
+    volumes => [
+      "${data_dir}:/var/repos/.pulp",
+      "/var/run/postgresql:/var/run/postgresql",
+      "/var/run/redis:/var/run/redis",
     ],
+    ports => ['24817:24817'],
+    username => '1200',
+    command => 'pulpcore-manager runserver 0.0.0.0:24817',
+    require => [
+      Docker::Image['pulp_image'],
+      Exec['pulp_admin_pw'],
+      Exec['pulp_collect_static'],
+      File[$data_dir],
+      File["${data_dir}/media"],
+      User['pulp'],
+    ],
+    depend_services => ['postgresql', 'redis-server'],
+  }
+
+  docker::run {'pulp_content':
+    image => 'pulp_image',
+    restart_service => true,
+    volumes => [
+      "${data_dir}:/var/repos/.pulp",
+      "/var/run/postgresql:/var/run/postgresql",
+      "/var/run/redis:/var/run/redis",
+    ],
+    ports => ['24816:24816'],
+    username => '1200',
+    command => 'pulp-content',
+    require => [
+      Docker::Image['pulp_image'],
+      Exec['pulp_admin_pw'],
+      Exec['pulp_collect_static'],
+      File[$data_dir],
+      User['pulp'],
+    ],
+    depend_services => ['postgresql', 'redis-server'],
+  }
+
+  docker::run {'pulp_rm':
+    image => 'pulp_image',
+    restart_service => true,
+    volumes => [
+      "${data_dir}:/var/repos/.pulp",
+      "/var/run/postgresql:/var/run/postgresql",
+      "/var/run/redis:/var/run/redis",
+    ],
+    username => '1200',
+    command => "rq worker -n 'resource-manager' -w 'pulpcore.tasking.worker.PulpWorker' -c 'pulpcore.rqconfig'",
+    require => [
+      Docker::Image['pulp_image'],
+      Exec['pulp_admin_pw'],
+      Exec['pulp_collect_static'],
+      File[$data_dir],
+      User['pulp'],
+    ],
+    depend_services => ['postgresql', 'redis-server'],
+  }
+
+  ['1', '2'].each |String $worker_id| {
+    docker::run {"pulp_worker_${worker_id}":
+      image => 'pulp_image',
+      restart_service => true,
+      volumes => [
+        "${data_dir}:/var/repos/.pulp",
+        "/var/run/postgresql:/var/run/postgresql",
+        "/var/run/redis:/var/run/redis",
+      ],
+      username => '1200',
+      command => "rq worker -n pulp_worker_${worker_id} -w 'pulpcore.tasking.worker.PulpWorker' -c 'pulpcore.rqconfig'",
+      require => [
+        Docker::Image['pulp_image'],
+        Exec['pulp_admin_pw'],
+        Exec['pulp_collect_static'],
+        File[$data_dir],
+        User['pulp'],
+      ],
+      depend_services => ['postgresql', 'redis-server'],
+    }
   }
 }
--- modules/pulp/manifests/init.pp
+++ modules/pulp/manifests/init.pp
@@ -1,23 +1,4 @@
 class pulp {
-  file { '/tmp/pulp':
-    ensure => 'directory',
-    mode   => '0700',
-  }
-
-  file { '/tmp/pulp/Dockerfile':
-    ensure => 'file',
-    mode   => '0600',
-    source => 'puppet:///modules/pulp/Dockerfile',
-    require => File['/tmp/pulp'],
-  }
-
-  file { '/tmp/pulp/entry_point.sh':
-    ensure => 'file',
-    mode   => '0755',
-    source => 'puppet:///modules/pulp/entry_point.sh',
-    require => File['/tmp/pulp'],
-  }
-
   file { '/tmp/pulp_rpm_repo':
     ensure => 'directory',
     mode   => '0700',
